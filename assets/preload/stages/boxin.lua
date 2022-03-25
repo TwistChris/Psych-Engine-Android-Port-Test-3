41aa938005a67a4477809f04394e169de9992df1
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-03-25 23:30:20 +0100

    Initial commit

---
+++ assets/preload/stages/boxin.lua
@@ -0,0 +1,13 @@
+function onCreate()
+
+	makeLuaSprite('boxback','bg_boxn',-750,-430)
+	addLuaSprite('boxback',false)
+	setLuaSpriteScrollFactor('boxback', 0.9, 0.9);
+
+	makeLuaSprite('boxbackalt','bg_boxr',-750,-430)
+	addLuaSprite('boxbackalt',false)
+	setLuaSpriteScrollFactor('boxbackalt', 0.9, 0.9);
+
+
+
+end
---
+++ source/CoolUtil.hx
@@ -0,0 +1,138 @@
+package;
+
+import flixel.FlxG;
+import openfl.utils.Assets;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+import lime.utils.Assets as LimeAssets;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import openfl.utils.Assets;
+
+using StringTools;
+
+class CoolUtil
+{
+	// [Difficulty name, Chart file suffix]
+	public static var difficultyStuff:Array<Dynamic> = [
+		['Easy', '-easy'],
+		['Normal', ''],
+		['Hard', '-hard']
+	];
+
+	public static function difficultyString():String
+	{
+		return difficultyStuff[PlayState.storyDifficulty][0].toUpperCase();
+	}
+
+	public static function boundTo(value:Float, min:Float, max:Float):Float {
+		var newValue:Float = value;
+		if(newValue < min) newValue = min;
+		else if(newValue > max) newValue = max;
+		return newValue;
+	}
+
+	public static function coolTextFile(path:String):Array<String>
+	{
+		var daList:Array<String> = [];
+		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+
+        public static function coolIntFile(path:String)
+	{
+		var daList:Array<String> = [];
+		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+        //get the most used color in an image
+	public static function dominantColor(sprite:FlxSprite):Int{
+		var countByColor:Map<Int, Int> = [];
+		for(col in 0...sprite.frameWidth){
+			for(row in 0...sprite.frameHeight){
+			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+			  if(colorOfThisPixel != 0){
+				  if(countByColor.exists(colorOfThisPixel)){
+				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+					 countByColor[colorOfThisPixel] = 1;
+				  }
+			  }
+			}
+		 }
+		var maxCount = 0;
+		var maxKey:Int = 0;//after the loop this will store the max color
+		countByColor[FlxColor.BLACK] = 0;
+			for(key in countByColor.keys()){
+			if(countByColor[key] >= maxCount){
+				maxCount = countByColor[key];
+				maxKey = key;
+			}
+		}
+		return maxKey;
+	}
+	
+        public static function leastColor(sprite:FlxSprite) //return the least used color in an image
+	{
+		var countByColor:Map<Int, Int> = [];
+		for(col in 0...sprite.frameWidth){
+			for(row in 0...sprite.frameHeight){
+			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+			  if(colorOfThisPixel != 0){
+				  if(countByColor.exists(colorOfThisPixel)){
+				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+					 countByColor[colorOfThisPixel] = 1;
+				  }
+			  }
+			}
+		 }
+		var minCount = 0;
+		var minKey:Int = 0;//after the loop this will store the min color
+		countByColor[FlxColor.BLACK] = 0;
+			for(key in countByColor.keys()){
+			if(countByColor[key] <= minCount){
+				minCount = countByColor[key];
+				minKey = key;
+			}
+		}
+		return minKey;
+	}
+
+	public static function numberArray(max:Int, ?min = 0):Array<Int>
+	{
+		var dumbArray:Array<Int> = [];
+		for (i in min...max)
+		{
+			dumbArray.push(i);
+		}
+		return dumbArray;
+	}
+
+	//uhhhh does this even work at all? i'm starting to doubt
+	public static function precacheSound(sound:String, ?library:String = null):Void {
+		if(!Assets.cache.hasSound(Paths.sound(sound, library))) {
+			FlxG.sound.cache(Paths.sound(sound, library));
+		}
+	}
+
+	public static function browserLoad(site:String) {
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [site]);
+		#else
+		FlxG.openURL(site);
+		#end
+	}
+}
